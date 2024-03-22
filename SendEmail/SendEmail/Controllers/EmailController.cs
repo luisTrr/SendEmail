684efed3218f451d5d1582b168bdd2ff2e217407
luisTrr <luistorrez997@gmail.com>
2024-03-22 00:20:04 -0400

    Send Email

---
+++ SendEmail/SendEmail/Controllers/EmailController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SendEmail.Models;
+using SendEmail.Services;
+
+namespace SendEmail.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmailController : ControllerBase
+    {
+        private readonly IEmailService _emailService;
+        public EmailController(IEmailService emailService)
+        {
+            _emailService = emailService;
+        }
+
+        [HttpPost]
+        public IActionResult SendEmail(EmailDTO request)
+        {
+            try
+            {
+                _emailService.SendEmail(request);
+                return Ok("Correo electrónico enviado correctamente.");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Error al enviar el correo electrónico: " + ex.Message);
+            }
+        }
+    }
+}
---
+++ SendEmail/SendEmail/Services/IEmailService.cs
@@ -0,0 +1,9 @@
+﻿using SendEmail.Models;
+
+namespace SendEmail.Services
+{
+    public interface IEmailService
+    {
+        void SendEmail(EmailDTO request);
+    }
+}
